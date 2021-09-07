aea34137a4f07b7af5575b49e0c777665e0adafc
dodid <dodid@outlook.com>
2021-09-07 17:50:11 +0800

    add solution

--- sm2-main.cpp
+++ sm2-main.cpp
@@ -9,84 +9,114 @@ using namespace std;
 #include <pcl/filters/voxel_grid.h>
 #include "helper.h"
 #include <sstream>
-#include <chrono> 
-#include <ctime> 
+#include <chrono>
+#include <ctime>
 #include <pcl/registration/icp.h>
 #include <pcl/registration/ndt.h>
 #include <pcl/console/time.h>
 
-enum Registration{ Off, Ndt};
+enum Registration
+{
+	Off,
+	Ndt
+};
 Registration matching = Off;
 
-Pose pose(Point(0,0,0), Rotate(0,0,0));
+Pose pose(Point(0, 0, 0), Rotate(0, 0, 0));
 Pose savedPose = pose;
-void keyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void* viewer)
+void keyboardEventOccurred(const pcl::visualization::KeyboardEvent &event, void *viewer)
 {
-	if (event.getKeySym() == "Right" && event.keyDown()){
+	if (event.getKeySym() == "Right" && event.keyDown())
+	{
 		matching = Off;
 		pose.position.x += 0.1;
-  	}
-	else if (event.getKeySym() == "Left" && event.keyDown()){
+	}
+	else if (event.getKeySym() == "Left" && event.keyDown())
+	{
 		matching = Off;
 		pose.position.x -= 0.1;
-  	}
-  	else if (event.getKeySym() == "Up" && event.keyDown()){
+	}
+	else if (event.getKeySym() == "Up" && event.keyDown())
+	{
 		matching = Off;
 		pose.position.y += 0.1;
-  	}
-	else if (event.getKeySym() == "Down" && event.keyDown()){
+	}
+	else if (event.getKeySym() == "Down" && event.keyDown())
+	{
 		matching = Off;
 		pose.position.y -= 0.1;
-  	}
-	else if (event.getKeySym() == "k" && event.keyDown()){
+	}
+	else if (event.getKeySym() == "k" && event.keyDown())
+	{
 		matching = Off;
 		pose.rotation.yaw += 0.1;
-		while( pose.rotation.yaw > 2*pi)
-			pose.rotation.yaw -= 2*pi; 
-  	}
-	else if (event.getKeySym() == "l" && event.keyDown()){
+		while (pose.rotation.yaw > 2 * pi)
+			pose.rotation.yaw -= 2 * pi;
+	}
+	else if (event.getKeySym() == "l" && event.keyDown())
+	{
 		matching = Off;
 		pose.rotation.yaw -= 0.1;
-		while( pose.rotation.yaw < 0)
-			pose.rotation.yaw += 2*pi; 
-  	}
-	else if(event.getKeySym() == "n" && event.keyDown()){
+		while (pose.rotation.yaw < 0)
+			pose.rotation.yaw += 2 * pi;
+	}
+	else if (event.getKeySym() == "n" && event.keyDown())
+	{
 		matching = Ndt;
 	}
-	else if(event.getKeySym() == "space" && event.keyDown()){
+	else if (event.getKeySym() == "space" && event.keyDown())
+	{
 		matching = Off;
 		pose = savedPose;
 	}
-	else if(event.getKeySym() == "x" && event.keyDown()){
+	else if (event.getKeySym() == "x" && event.keyDown())
+	{
 		matching = Off;
 		savedPose = pose;
 	}
-
 }
 
-Eigen::Matrix4d NDT(pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> ndt, PointCloudT::Ptr source, Pose startingPose, int iterations){
+Eigen::Matrix4d NDT(pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> ndt, PointCloudT::Ptr source, Pose startingPose, int iterations)
+{
 
-  	Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity ();
+	Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity();
+	Eigen::Matrix4d initTransform = transform3D(startingPose.rotation.yaw, startingPose.rotation.pitch, startingPose.rotation.roll, startingPose.position.x, startingPose.position.y, startingPose.position.z);
 
-  	// TODO: Implement the PCL NDT function and return the correct transformation matrix
-  	// .....
-  	
-  	return transformation_matrix;
+	// TODO: Implement the PCL NDT function and return the correct transformation matrix
+	// Setting minimum transformation difference for termination condition.
+  	ndt.setTransformationEpsilon (.0001);
+	// Setting max number of registration iterations.
+	ndt.setMaximumIterations(iterations);
+	// Setting point cloud to be aligned.
+	ndt.setInputSource(source);
 
+	PointCloudT::Ptr output(new PointCloudT);
+	ndt.align(*output, initTransform.cast<float>());
+
+	if (ndt.hasConverged())
+	{
+		transformation_matrix = ndt.getFinalTransformation().cast<double>();
+	}
+	else
+		cout << "WARNING: NDT did not converge" << endl;
+
+	return transformation_matrix;
 }
 
-void drawCar(Pose pose, int num, Color color, double alpha, pcl::visualization::PCLVisualizer::Ptr& viewer){
+void drawCar(Pose pose, int num, Color color, double alpha, pcl::visualization::PCLVisualizer::Ptr &viewer)
+{
 
 	BoxQ box;
 	box.bboxTransform = Eigen::Vector3f(pose.position.x, pose.position.y, 0);
-    box.bboxQuaternion = getQuaternion(pose.rotation.yaw);
-    box.cube_length = 4;
-    box.cube_width = 2;
-    box.cube_height = 2;
+	box.bboxQuaternion = getQuaternion(pose.rotation.yaw);
+	box.cube_length = 4;
+	box.cube_width = 2;
+	box.cube_height = 2;
 	renderBox(viewer, box, num, color, alpha);
 }
 
-struct Tester{
+struct Tester
+{
 
 	Pose pose;
 	bool init = true;
@@ -100,22 +130,26 @@ struct Tester{
 	vector<double> distHistory;
 	vector<double> angleHistory;
 
-	void Reset(){
-		cout << "Total time: " << timer.toc () << " ms, Total cycles: " << cycles << endl;
+	void Reset()
+	{
+		cout << "Total time: " << timer.toc() << " ms, Total cycles: " << cycles << endl;
 		init = true;
 		cycles = 0;
 		distHistory.clear();
 		angleHistory.clear();
 	}
 
-	double angleMag( double angle){
+	double angleMag(double angle)
+	{
 
-		return abs(fmod(angle+pi, 2*pi) - pi);
+		return abs(fmod(angle + pi, 2 * pi) - pi);
 	}
 
-	bool Displacement( Pose p){
+	bool Displacement(Pose p)
+	{
 
-		if(init){
+		if (init)
+		{
 			timer.tic();
 			pose = p;
 			init = false;
@@ -124,9 +158,10 @@ struct Tester{
 
 		Pose movement = p - pose;
 		double tdist = sqrt(movement.position.x * movement.position.x + movement.position.y * movement.position.y + movement.position.z * movement.position.z);
-		double adist = max( max( angleMag(movement.rotation.yaw), angleMag(movement.rotation.pitch)), angleMag(movement.rotation.roll) );
+		double adist = max(max(angleMag(movement.rotation.yaw), angleMag(movement.rotation.pitch)), angleMag(movement.rotation.roll));
 
-		if(tdist > distThresh || adist > angleThresh){
+		if (tdist > distThresh || adist > angleThresh)
+		{
 			distHistory.push_back(tdist);
 			angleHistory.push_back(adist);
 			pose = p;
@@ -136,73 +171,82 @@ struct Tester{
 		}
 		else
 			return false;
-	
 	}
-
 };
 
-int main(){
+int main()
+{
 
-	pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-  	viewer->setBackgroundColor (0, 0, 0);
-	viewer->registerKeyboardCallback(keyboardEventOccurred, (void*)&viewer);
-	viewer->setCameraPosition(pose.position.x, pose.position.y, 60, pose.position.x+1, pose.position.y+1, 0, 0, 0, 1);
+	pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+	viewer->setBackgroundColor(0, 0, 0);
+	viewer->registerKeyboardCallback(keyboardEventOccurred, (void *)&viewer);
+	viewer->setCameraPosition(pose.position.x, pose.position.y, 60, pose.position.x + 1, pose.position.y + 1, 0, 0, 0, 1);
 
 	// Load map and display it
 	PointCloudT::Ptr mapCloud(new PointCloudT);
-  	pcl::io::loadPCDFile("map.pcd", *mapCloud);
-  	cout << "Loaded " << mapCloud->points.size() << " data points from map.pcd" << endl;
-	renderPointCloud(viewer, mapCloud, "map", Color(0,0,1)); 
+	pcl::io::loadPCDFile("map.pcd", *mapCloud);
+	cout << "Loaded " << mapCloud->points.size() << " data points from map.pcd" << endl;
+	renderPointCloud(viewer, mapCloud, "map", Color(0, 0, 1));
 
 	// True pose for the input scan
-	vector<Pose> truePose ={Pose(Point(2.62296,0.0384164,0), Rotate(6.10189e-06,0,0)), Pose(Point(4.91308,0.0732088,0), Rotate(3.16001e-05,0,0))};
-	drawCar(truePose[0], 0,  Color(1,0,0), 0.7, viewer);
+	vector<Pose> truePose = {Pose(Point(2.62296, 0.0384164, 0), Rotate(6.10189e-06, 0, 0)), Pose(Point(4.91308, 0.0732088, 0), Rotate(3.16001e-05, 0, 0))};
+	drawCar(truePose[0], 0, Color(1, 0, 0), 0.7, viewer);
 
 	// Load input scan
 	PointCloudT::Ptr scanCloud(new PointCloudT);
-  	pcl::io::loadPCDFile("scan1.pcd", *scanCloud);
+	pcl::io::loadPCDFile("scan1.pcd", *scanCloud);
 
-	typename pcl::PointCloud<PointT>::Ptr cloudFiltered (new pcl::PointCloud<PointT>);
+	typename pcl::PointCloud<PointT>::Ptr cloudFiltered(new pcl::PointCloud<PointT>);
 
-	cloudFiltered = scanCloud; // TODO: remove this line
 	//TODO: Create voxel filter for input scan and save to cloudFiltered
-	// ......
+	pcl::VoxelGrid<PointT> vg;
+	float res = 0.5;
+	vg.setInputCloud(scanCloud);
+	vg.setLeafSize(res, res, res);
+	vg.filter(*cloudFiltered);
 
 	pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> ndt;
 	//TODO: Set resolution and point cloud target (map) for ndt
-	// ......
+	// Setting maximum step size for More-Thuente line search.
+	ndt.setStepSize(1);
+	//Setting Resolution of NDT grid structure (VoxelGridCovariance).
+	ndt.setResolution(1);
+	// Setting point cloud to be aligned to.
+	ndt.setInputTarget(mapCloud);
 
-	PointCloudT::Ptr transformed_scan (new PointCloudT);
+	PointCloudT::Ptr transformed_scan(new PointCloudT);
 	Tester tester;
 
 	while (!viewer->wasStopped())
-  	{
+	{
 		Eigen::Matrix4d transform = transform3D(pose.rotation.yaw, pose.rotation.pitch, pose.rotation.roll, pose.position.x, pose.position.y, pose.position.z);
 
-		if( matching != Off){
-			if( matching == Ndt)
-				transform = NDT(ndt, cloudFiltered, pose, 0); //TODO: change the number of iterations to positive number
-  			pose = getPose(transform);
-			if( !tester.Displacement(pose) ){
-				if(matching == Ndt)
+		if (matching != Off)
+		{
+			if (matching == Ndt)
+				transform = NDT(ndt, cloudFiltered, pose, 100); //TODO: change the number of iterations to positive number
+			pose = getPose(transform);
+			if (!tester.Displacement(pose))
+			{
+				if (matching == Ndt)
 					cout << " Done testing NDT" << endl;
 				tester.Reset();
-				double pose_error = sqrt( (truePose[0].position.x - pose.position.x) * (truePose[0].position.x - pose.position.x) + (truePose[0].position.y - pose.position.y) * (truePose[0].position.y - pose.position.y) );
+				double pose_error = sqrt((truePose[0].position.x - pose.position.x) * (truePose[0].position.x - pose.position.x) + (truePose[0].position.y - pose.position.y) * (truePose[0].position.y - pose.position.y));
 				cout << "pose error: " << pose_error << endl;
 				matching = Off;
 			}
 		}
-		
-  		pcl::transformPointCloud (*cloudFiltered, *transformed_scan, transform);
+
+		pcl::transformPointCloud(*cloudFiltered, *transformed_scan, transform);
 		viewer->removePointCloud("scan");
-		renderPointCloud(viewer, transformed_scan, "scan", Color(1,0,0)	);
+		renderPointCloud(viewer, transformed_scan, "scan", Color(1, 0, 0));
 
 		viewer->removeShape("box1");
 		viewer->removeShape("boxFill1");
-		drawCar(pose, 1,  Color(0,1,0), 0.35, viewer);
-		
-  		viewer->spinOnce ();
-  	}
+		drawCar(pose, 1, Color(0, 1, 0), 0.35, viewer);
+
+		viewer->spinOnce();
+	}
 
 	return 0;
 }
